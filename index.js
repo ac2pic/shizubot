3baf78afbfc61f8c1e14934628a6a4a2720ae5ad
ac2pic <ac2pic@gmail.com>
2018-12-06 21:20:14 -0600

    Made this compatiable with heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const {Client} = require('discord.js');
 
 // TODO: setup heroku
-const token = JSON.parse(require('fs').readFileSync('.env')).token;
 const client = new Client();
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`This is the updated file. :p`);
 });
 
 client.on(`message`, msg => {
@@ -15,4 +15,4 @@ client.on(`message`, msg => {
 	console.log("Received new message:", msg.content);
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
